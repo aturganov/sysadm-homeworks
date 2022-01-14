9282a9c86023cb98460640540524e4b5d0692e70
Artem <aturganov@gmail.com>
2022-01-14 18:12:38 +0300

    add 4_3_1

---
+++ 04-script-03-yaml/4_3_1.py
@@ -0,0 +1,19 @@
+import json
+
+json_str = '''
+    { "info" : "Sample JSON output from our service\t",
+        "elements" :[
+            { "name" : "first",
+            "type" : "server",
+            "ip" : 7175 
+            },
+            { "name" : "second",
+            "type" : "proxy",
+            "ip" : "71.78.22.43"
+            }
+        ]
+    }
+'''
+# Check
+json_dump = json.dumps(json_str)
+print(str(json_dump))

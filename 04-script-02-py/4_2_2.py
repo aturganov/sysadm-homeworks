71558072df4b3d0966d6b2fa064ed7a6e8bf474b
Artem <aturganov@gmail.com>
2022-01-15 14:26:52 +0300

    4_1

--- 04-script-02-py/4_2_2.py
+++ 04-script-02-py/4_2_2.py
@@ -1,21 +1,21 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 
-    import os
-    from pathlib import Path
+import os
+from pathlib import Path
 
-    # bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
-    # специфика windows машины
+# bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
+# специфика windows машины
 
-    folder_check = str(Path.home()) + "\\Documents\\netology\\sysadm-homeworks"
-    bash_command = ["cd " + folder_check, "git status"]
+folder_check = str(Path.home()) + "\\Documents\\netology\\sysadm-homeworks"
+bash_command = ["cd " + folder_check, "git status"]
 
-    result_os = os.popen(' && '.join(bash_command)).read()
-    is_change = False
+result_os = os.popen(' && '.join(bash_command)).read()
+is_change = False
 
-    for result in result_os.split('\n'):
-        if result.find('modified') != -1:
-            prepare_result = result.replace('\tmodified:   ', '')
-            # Добавляем полный путь
-            print(folder_check + '\\' + prepare_result)
-            # Убираем break, чтоб видеть все изменения
-            # break
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        # Добавляем полный путь
+        print(folder_check + '\\' + prepare_result)
+        # Убираем break, чтоб видеть все изменения
+        # break
